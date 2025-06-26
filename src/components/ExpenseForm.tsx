12cf1fdbc70092d78e83239eb7b2b81575e4e707
YashdeepN <yashdeepneema@hotmail.com>
2025-06-26 00:54:27 -0500

    expense form validation with zod

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,30 +1,68 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const expenseFormSchema = z.object({
+  description: z
+    .string()
+    .min(3, { message: "description should at least be 3 characters!" }),
+  amount: z.number().min(1),
+  category: z.string().min(4),
+});
+
+type FormData = z.infer<typeof expenseFormSchema>;
+
 const ExpenseForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(expenseFormSchema) });
+
   return (
-    <form>
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
         </label>
-        <input type="text" id="description" className="form-control" />
+        <input
+          {...register("description")}
+          type="text"
+          id="description"
+          className="form-control"
+        />
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
           Amount
         </label>
-        <input type="number" id="amount" className="form-control" />
+        <input
+          {...register("amount", { valueAsNumber: true })}
+          type="number"
+          id="amount"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="category" className="form-label">
           Category
         </label>
-        <select name="category" id="category" className="form-select">
+        <select
+          {...register("category")}
+          name="category"
+          id="category"
+          className="form-select"
+        >
           <option></option>
-          <option>Groceries</option>
-          <option>Utilities</option>
-          <option>Entertainment</option>
+          <option value="some">Groceries</option>
+          <option value="some">Utilities</option>
+          <option value="some">Entertainment</option>
         </select>
       </div>
-      <button type="submit" className="btn btn-primary">
+      <button disabled={!isValid} type="submit" className="btn btn-primary">
         Add
       </button>
     </form>
